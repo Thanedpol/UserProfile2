a1dee5c67aeacbb49d536294f28b9a184a5370d7
Thanedpol <Thanedpol@outlook.co.th>
2023-09-03 14:05:15 +0700

    Update header.js

--- src/contents/header.js
+++ src/contents/header.js
@@ -2,7 +2,7 @@ import flieName from '../assets/Resume 4.18 FrontEnd Developer.pdf'
 
 export const data = {
     name:"Thanedpol Dechaduangsakul",
-    title:"Programmer . between jobs",
+    title:"UX/UI Designer . between jobs",
     caption:"Hi, I'm Thank. I created my profile digital from ideas to reality with code, transforming imagination ideas into online.",
     btnText:"View Resume",
     link: flieName,
